d06adc3ea00ba50f9d479a6c991ca769faf934ff
kenier <kenier202@gmail.com>
2024-10-21 15:52:40 -0500

    feat(http country): add query to fetch countrys

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -15,7 +15,7 @@ import { RouterModule } from '@angular/router';
     AppRoutingModule,
     SharedModule,
     AppRoutingModule,
-
+    HttpClientModule
   ],
   exports:[
   ],
