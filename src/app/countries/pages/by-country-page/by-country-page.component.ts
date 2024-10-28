daf7dd1fb5b53484180463e29017abab6647d6bd
kenier <kenier202@gmail.com>
2024-10-28 10:02:33 -0500

    feat(country): set initialvalue and search country data persistence

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
@@ -7,10 +7,17 @@ import { Country } from '../../interfaces/country';
   templateUrl: './by-country-page.component.html',
   styles: ``
 })
-export class ByCountryPageComponent {
+export class ByCountryPageComponent implements OnInit{
 
   public countries : Country[] = [];
+  public initialValue: string = '';
+
   constructor(public CountriesService: CountriesService){}
+
+  ngOnInit(): void {
+    this.countries = this.CountriesService.cacheStore.byName.countries;
+    this.initialValue = this.CountriesService.cacheStore.byName.term;
+  }
   searchByCountry(term: string){
     this.CountriesService.searchCountry(term).subscribe(countries => this.countries = countries);
   }
